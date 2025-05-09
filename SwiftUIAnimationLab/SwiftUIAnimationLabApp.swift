ea4d8c7dc3170a7e3eece43de98dcacd44c53c5a
Denidu Gamage <denidugamage21@gmail.com>
2025-05-09 15:36:08 +0530

    Initial Commit

---
+++ SwiftUIAnimationLab/SwiftUIAnimationLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationLabApp.swift
+//  SwiftUIAnimationLab
+//
+//  Created by Denidu Gamage on 2025-05-09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIAnimationLabTests/SwiftUIAnimationLabTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationLabTests.swift
+//  SwiftUIAnimationLabTests
+//
+//  Created by Denidu Gamage on 2025-05-09.
+//
+
+import Testing
+@testable import SwiftUIAnimationLab
+
+struct SwiftUIAnimationLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
